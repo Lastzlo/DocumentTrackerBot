eb5ac64e43f6adbf8519a10f76f5574d02a9c5da
Lastzlo <z.last.zlo@gmail.com>
2022-02-05 23:45:28 +0200

    git init

---
+++ src/main/java/lastzlo/doctrackerbot/DocTrackerBotApplication.java
@@ -0,0 +1,13 @@
+package lastzlo.doctrackerbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocTrackerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocTrackerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lastzlo/doctrackerbot/DocTrackerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package lastzlo.doctrackerbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocTrackerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
