7c7989d671c036d9c7ef25d268a2fbe4596a6203
Lastzlo <z.last.zlo@gmail.com>
2022-02-10 11:40:21 +0200

    refactor add getAll

--- src/main/java/lastzlo/doctrackerbot/service/DocumentService.java
+++ src/main/java/lastzlo/doctrackerbot/service/DocumentService.java
@@ -5,6 +5,8 @@ import lastzlo.doctrackerbot.repository.DocumentRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DocumentService {
 
@@ -23,56 +25,11 @@ public class DocumentService {
 		return documentRepo.findByCaseNumber(caseNumber);
 	}
 
-//	public boolean addDocumentToUser(String caseNumber, AppUser user) {
-//		Document documentFromDb = documentRepo.findByCaseNumber(caseNumber);
-//		if (documentFromDb == null) {
-//			documentFromDb = documentRepo.save(
-//					new Document(caseNumber)
-//			);
-//
-//			return userDocumentService.addDocumentToUser(documentFromDb, user);
-//		} else {
-//			return userDocumentService.addExistsDocumentToUser(documentFromDb, user);
-//		}
-//	}
-//
-//	public boolean deleteDocumentFromUser(String input, AppUser user) {
-//		Document documentFromDb = documentRepo.findByCaseNumber(input);
-//
-//		if (documentFromDb == null) return false;
-//
-//		boolean isDeleteSuccess = userDocumentService.deleteDocumentFromUser(documentFromDb, user);
-//		if (! isDeleteSuccess) return false;
-//
-//		boolean hasUsersWithDocument = userDocumentService.hasUsersWithDocument(documentFromDb);
-//		if (! hasUsersWithDocument) documentRepo.delete(documentFromDb);
-//
-//		return true;
-//
-//
-////		if (documentFromDb == null) {
-////			return false;
-////		} else {
-////			boolean isDeleteSuccess = userDocumentService.deleteDocumentFromUser(documentFromDb, user);
-////
-////			if (isDeleteSuccess) {
-////				boolean hasUsersWithDocument = userDocumentService.hasUsersWithDocument(documentFromDb);
-////
-////				if (! hasUsersWithDocument) {
-////					documentRepo.delete(documentFromDb);
-////				}
-////
-////				return true;
-////			} else {
-////				return false;
-////			}
-////
-////		}
-//
-//
-//	}
-
 	public void deleteDocument(Document document) {
 		documentRepo.delete(document);
+	}
+
+	public List<Document> getAll() {
+		return documentRepo.findAll();
 	}
 }
